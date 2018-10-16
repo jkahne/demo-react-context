9613e1b94ae724774bf940e7ddabe85391e42dd6
Jeremy Kahne <jkahne@gmail.com>
2018-10-16 17:21:23 -0400

    basic todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Todo extends React.Component{
+  render(){
+    const todo = this.props.todo;
+    return(
+      <div>
+        <input
+          type="checkbox"
+          id={`todo_${todo.id}`}
+        />
+        <label>{todo.description}</label>
+      </div>
+    )
+  }
+}
+
+class Section extends React.Component{
+  render(){
+    return (
+      <div>
+        <div>{this.props.section.name}</div>
+        <div>
+          {this.props.section.todos.map(todo => (
+            <Todo
+              key={todo.id}
+              todo={todo}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    sections: [
+      {id: 1, name: 'groceries', todos: [
+        {id: 1, description: 'milk'},
+        {id: 2, description: 'bread'}
+      ]},
+      {id: 2, name: 'home', todos: [
+        {id: 3, description: 'cut grass'},
+        {id: 4, description: 'dishes'},
+        {id: 5, description: 'nap'}
+      ]},
+    ],
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.sections.map(section => (
+          <Section
+            key={section.id}
+            section={section}
+          />
+        ))}
       </div>
     );
   }
